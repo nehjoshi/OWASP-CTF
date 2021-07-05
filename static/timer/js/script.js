61e1fd28dab038b5faab96699f423621d4f7a3cc
Neh Joshi <nehjoshi5@gmail.com>
2021-07-05 17:51:03 +0530

    Fixed Bugs

--- static/timer/js/script.js
+++ static/timer/js/script.js
@@ -32,6 +32,7 @@
         }, 0)
     }());
 
+const modal = document.querySelector(".modal-container")
 const sidebar = document.querySelector(".sidebar-container");
 
 const handleNav = () => {
@@ -40,4 +41,15 @@ const handleNav = () => {
 }
 const handleClose = () => {
     sidebar.className="sidebar-container";
+}
+const modalControl = () => {
+    
+  sidebar.className="sidebar-container";
+  if (!modal.style.display || modal.style.display=="none"){
+      modal.style.display="flex";
+      console.log("called!");
+  }
+  else {
+      modal.style.display="none";
+  }
 }
