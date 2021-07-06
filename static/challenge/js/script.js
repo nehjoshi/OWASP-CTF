09c0b37a03f7898f8624dfc7b38f3ed0575fd7b4
Neh Joshi <nehjoshi5@gmail.com>
2021-07-06 21:29:06 +0530

    Style changes

--- static/challenge/js/script.js
+++ static/challenge/js/script.js
@@ -1,6 +1,5 @@
 const sidebar = document.querySelector(".sidebar-container");
-const modal = document.querySelector('.modal-container');
-modal.style.display="none";
+
 const handleNav = () => {
     console.log("Clicked!");
     sidebar.className="sidebar-container-active";
@@ -8,14 +7,14 @@ const handleNav = () => {
 const handleClose = () => {
     sidebar.className="sidebar-container";
 }
-const modalControl = () => {
+// const modalControl = () => {
     
-    sidebar.className="sidebar-container";
-    if (!modal.style.display || modal.style.display=="none"){
-        modal.style.display="flex";
-        console.log("called!");
-    }
-    else {
-        modal.style.display="none";
-    }
-}
+//     sidebar.className="sidebar-container";
+//     if (!modal.style.display || modal.style.display=="none"){
+//         modal.style.display="flex";
+//         console.log("called!");
+//     }
+//     else {
+//         modal.style.display="none";
+//     }
+// }
